d2858a0329ff3319f319777e516bbd237168237b
trus2lun <53042040+trus2lun@users.noreply.github.com>
2019-09-12 18:42:04 +0700

    Add files via upload

--- session17/js/btvn.js
+++ session17/js/btvn.js
@@ -1,24 +1,28 @@
-var slideIndex;
-function showSlides() {
-	var i;
-	var slides = document.getElementByClassName("first");
-	var dots = document.getElementByClassName("dot");
-	for (i = 0; i < slides.length; i++) {
-         slides[i].style.display = "none";  
-    }
-	for (i = 0; i < dots.length; i++) {
-		dots[i].className = dots[i].className.replace(" active", "");
-	}
+var slideIndex = 1;
+showSlides(slideIndex);
 
-	slides[slideIndex].style.display = "block";
-	dots[slideIndex].className += " active";
-	slideIndex ++;
-	if (slideIndex > slides.length - 1) {
-		slideIndex = 0;
-	}
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
-showSlides(slideIndex = 0);
 
+// Thumbnail image controls
 function currentSlide(n) {
-	showSlides(slideIndex = n);
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
