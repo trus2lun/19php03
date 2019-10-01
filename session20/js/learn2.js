16436aaf1104133279c6b51cfd479d0fa8f8a47e
trus2lun <53042040+trus2lun@users.noreply.github.com>
2019-10-01 16:51:34 +0700

    Add files via upload

--- session20/js/learn2.js
+++ session20/js/learn2.js
@@ -1,6 +1,22 @@
-$('#yes').dblclick(function () {
-	alert("yes");
+$('.hide').click(function () {
+	$('.demo').hide(3000);
 })
-$('#no').dblclick(function () {
-	alert("yes");
+$('.show').click(function () {
+	$('.demo').show(3000).css("color","blue");
 })
+$('.fadein').click(function () {
+	$('.demo').fadeIn("slow");
+})
+$('fadeout').click(function () {
+	$('.demo').fadeOut();
+})
+// slideDown()
+// slideUp()
+// slideToggle()
+
+$('.animation_right').click(function(){
+	$('.demo2').animate({'left':'60%'});
+});
+$('.animation_left').click(function(){
+	$('.demo2').animate({'left':'0%'})
+});
